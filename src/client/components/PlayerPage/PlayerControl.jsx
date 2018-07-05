6e4cb27abd7920edda06a289f93321623f54ef68
Logan Wilquin <Skullyfox.yt@gmail.com>
2018-07-05 17:27:51 +0200

    Player Controls added

--- src/client/components/PlayerPage/PlayerControl.jsx
+++ src/client/components/PlayerPage/PlayerControl.jsx
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 
 class PlayerControl extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            onListen: false,
+        }
+        this.playOrPause = this.playOrPause.bind(this)
+    }
+
+    playOrPause(){
+        if (this.state.onListen){
+            this.setState({'onListen' : false}) 
+        } else {
+            this.setState({'onListen' : true}) 
+        }
+    }
+
+
     render() {
         return (
             <div id="PlayerControl">
-                <div className="controls"></div>
+                <div className="controls">
+                    <div className="controlButton">
+                        <i className="fas fa-step-backward"></i>
+                    </div>
+                    <div className="controlButton" onClick={this.playOrPause}>
+                        <i className={"fas " + (this.state.onListen ? 'fa-pause' : 'fa-play')}></i>
+                    </div>
+                    <div className="controlButton">
+                        <i className="fas fa-stop"></i>
+                    </div>
+                    <div className="controlButton">
+                        <i className="fas fa-step-forward"></i>
+                    </div>
+                </div>
             </div>
         );
     }
